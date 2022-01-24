2c3847332bb2a3e7d4a4b123b6c2c68fc6d125e2
Mohit Dhattarwal <mohit@facings.io>
2022-01-24 16:08:20 +0530

    [test] test cases fixed

--- tests/mycalculatordapp.js
+++ tests/mycalculatordapp.js
@@ -41,7 +41,7 @@ describe('mycalculatordapp', () => {
   it('Multiplies two numbers', async function() {
     const calculator = _calculator;
     
-    await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+    await program.rpc.multiply(new anchor.BN(2), new anchor.BN(3), {
       accounts: {
         calculator: calculator.publicKey,
       },
@@ -55,7 +55,7 @@ describe('mycalculatordapp', () => {
   it('Subtracts two numbers', async function() {
     const calculator = _calculator;
     
-    await program.rpc.add(new anchor.BN(4), new anchor.BN(3), {
+    await program.rpc.subtract(new anchor.BN(4), new anchor.BN(3), {
       accounts: {
         calculator: calculator.publicKey,
       },
@@ -69,7 +69,7 @@ describe('mycalculatordapp', () => {
   it('Divides two numbers', async function() {
     const calculator = _calculator;
     
-    await program.rpc.add(new anchor.BN(7), new anchor.BN(3), {
+    await program.rpc.divide(new anchor.BN(7), new anchor.BN(3), {
       accounts: {
         calculator: calculator.publicKey,
       },
